1d020ef83aa1686398eaae3c7041a68666e45dc9
nhiratani <N.Hiratani@gmail.com>
2024-05-01 09:48:08 -0500

    Update and rename tlcf1_lst2_cg.py to lst_cg.py

--- teacher-student/lst_cg.py
+++ teacher-student/lst_cg.py
@@ -13,7 +13,7 @@ import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
 
-from tlcf1_lst2_model import generate_tasks, calc_dW_cg, fnorm2, generate_g
+from lst_model import generate_tasks, calc_dW_cg, fnorm2, generate_g
 
 import matplotlib.pyplot as plt
 
