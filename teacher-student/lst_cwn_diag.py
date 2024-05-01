a745573d3110b8d1d24a9fc4cfef62dc43760cea
nhiratani <N.Hiratani@gmail.com>
2024-05-01 09:54:26 -0500

    Update and rename tlcf1_lst2_cwn_diag.py to lst_cwn_diag.py

--- teacher-student/lst_cwn_diag.py
+++ teacher-student/lst_cwn_diag.py
@@ -13,7 +13,7 @@ import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
 
-from tlcf1_lst2_model import generate_tasks, calc_dW, calc_dW_wn, calc_dW_cwn_diag, fnorm2
+from lst_model import generate_tasks, calc_dW, calc_dW_wn, calc_dW_cwn_diag, fnorm2
 
 	
 def run_exp_vnl_cwn_diag(params):
@@ -181,6 +181,5 @@ if __name__ == "__main__":
 	#simul_set1(params)
 	#simul_set2(params)
 	simul_set3(params)
-	#simul_set4(params)
 		
 		
